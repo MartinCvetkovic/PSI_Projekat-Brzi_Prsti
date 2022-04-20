6b0873101d04378aa20c3c9e9affd65fabf0bdd6
tp39-19Student <tp190039d@student.etf.bg.ac.rs>
2022-04-20 16:12:07 +0200

    Dodata skripta za pravljenje baze podataka

---
+++ ModelBazePodataka/SkriptaBaze.sql
@@ -0,0 +1,114 @@
+DROP SCHEMA IF EXISTS brziprsti;
+CREATE SCHEMA brziprsti;
+USE brziprsti;
+
+CREATE TABLE DnevnaRangLista
+(
+	id                   INTEGER NOT NULL,
+	vreme                FLOAT NULL,
+	idKor                INTEGER NOT NULL,
+	idTekst              INTEGER NOT NULL
+);
+
+ALTER TABLE DnevnaRangLista
+ADD CONSTRAINT XPKDnevnaRangLista PRIMARY KEY (id);
+
+
+CREATE TABLE DnevniIzazov
+(
+	id                   INTEGER NOT NULL,
+	sadrzaj              VARCHAR(4000) NULL,
+	tezina               FLOAT NULL,
+	nazivKategorije      VARCHAR(20) NULL,
+	idTekst              INTEGER NOT NULL
+);
+
+ALTER TABLE DnevniIzazov
+ADD CONSTRAINT XPKDnevniIzazov PRIMARY KEY (id);
+
+
+CREATE TABLE jePrijatelj
+(
+	id                   INTEGER NOT NULL,
+	idKor1               INTEGER NOT NULL,
+	idKor2               INTEGER NOT NULL
+);
+
+ALTER TABLE jePrijatelj
+ADD CONSTRAINT XPKjePrijatelj PRIMARY KEY (id);
+
+
+CREATE TABLE Kategorija
+(
+	id                   INTEGER NOT NULL,
+	naziv                VARCHAR(20) NULL
+);
+
+ALTER TABLE Kategorija
+ADD CONSTRAINT XPKKategorija PRIMARY KEY (id);
+
+
+CREATE TABLE Korisnik
+(
+	id                   INTEGER NOT NULL,
+	username             VARCHAR(20) NULL,
+	password             VARCHAR(100) NULL,
+	zlato                INTEGER NULL,
+	srebro               INTEGER NULL,
+	bronza               INTEGER NULL,
+	tip                  INTEGER NULL,
+	aktivan              INTEGER NULL,
+	brojPrijatelja       INTEGER NULL
+);
+
+ALTER TABLE Korisnik
+ADD CONSTRAINT XPKKorisnik PRIMARY KEY (id);
+
+
+CREATE TABLE RangLista
+(
+	id                   INTEGER NOT NULL,
+	vreme                FLOAT NULL,
+	idKor                INTEGER NOT NULL,
+	idTekst              INTEGER NOT NULL
+);
+
+ALTER TABLE RangLista
+ADD CONSTRAINT XPKRangLista PRIMARY KEY (id);
+
+
+CREATE TABLE Tekst
+(
+	id                   INTEGER NOT NULL,
+	sadrzaj              VARCHAR(4000) NULL,
+	tezina               FLOAT NULL,
+	idKat                INTEGER NOT NULL
+);
+
+ALTER TABLE Tekst
+ADD CONSTRAINT XPKTekst PRIMARY KEY (id);
+
+
+ALTER TABLE DnevnaRangLista
+ADD CONSTRAINT R_20 FOREIGN KEY (idKor) REFERENCES Korisnik (id);
+
+ALTER TABLE DnevnaRangLista
+ADD CONSTRAINT R_21 FOREIGN KEY (idTekst) REFERENCES Tekst (id);
+
+ALTER TABLE DnevniIzazov
+ADD CONSTRAINT R_22 FOREIGN KEY (idTekst) REFERENCES Tekst (id);
+
+ALTER TABLE jePrijatelj
+ADD CONSTRAINT R_15 FOREIGN KEY (idKor1) REFERENCES Korisnik (id);
+
+ALTER TABLE jePrijatelj
+ADD CONSTRAINT R_16 FOREIGN KEY (idKor2) REFERENCES Korisnik (id);
+
+ALTER TABLE RangLista
+ADD CONSTRAINT R_17 FOREIGN KEY (idKor) REFERENCES Korisnik (id);
+
+ALTER TABLE RangLista
+ADD CONSTRAINT R_18 FOREIGN KEY (idTekst) REFERENCES Tekst (id);
+
+ALTER TABLE Tekst
+ADD CONSTRAINT R_2 FOREIGN KEY (idKat) REFERENCES Kategorija (id); 
