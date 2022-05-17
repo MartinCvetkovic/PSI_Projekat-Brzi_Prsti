e008bee5e526e4467173118ae507dadbb6548701
Aleksa Savić <10512545+aleksamagicka@users.noreply.github.com>
2022-05-17 11:21:47 +0200

    Tekstovi: prebačeni na latinicu (#2)
    * UserController: Tekst prebačen na latinicu i centrirano dugme za početak solo kucanja
    
    * template.blade: Dodat Jquery 3.6.0

--- Implementacija/resources/views/template.blade.php
+++ Implementacija/resources/views/template.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href={{asset("css/templateStyle.css")}}>
     <style ></style>
     <title>Brzi prsti</title>
