987ff32bfa8105d3845ddfcb8d7634d785a0002c
ikilis <ivanthesavic@gmail.com>
2022-05-17 20:14:27 +0200

    Zavrsen skelet kod pregleda profila uz komentare

--- Implementacija/resources/views/profile.blade.php
+++ Implementacija/resources/views/profile.blade.php
@@ -1,9 +1,12 @@
+{{-- Иван Савић 2019-0389 Страница за преглед профила --}}
+
 @extends("template")
 
 @section("content")
 
 <div class="row  mx-4  d-flex align-items-center">
 
+    
     <div class="col-sm-8">
         <ul class="list-group bg-light">
             <li class="list-group-item d-flex justify-content-between bg-light"> 
@@ -13,9 +16,14 @@
             </li>
 
     </div>
+    {{-- Преглед сопственог профила нема додатне дугмиће --}}
     <div class="col-sm-4">
-
-        <button type="button" class="btn btn-primary ">Dodaj</button>
+        {{-- 
+        Сви
+        Админ Мод
+        Админ 
+        --}}
+        <button type="button" class="btn btn-primary ">Dodaj</button>   
         <button type="button" class="btn btn-danger mx-4">Blokiraj</button>
         <button type="button" class="btn btn-warning ">Mod</button>
     </div>
@@ -24,7 +32,7 @@
 <div class="row pt-2  mx-4 d-flex align-items-center">
     <div class="col-sm-8 ">
 
-        <div class="list-group  overflow-auto" style="max-height:200px">
+        <div class="list-group  overflow-auto border-top border-bottom border-black" style="max-height:200px">
                 
                 <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between">
                     <p style="margin-bottom:0px"> Prijatelj 1</p>
@@ -54,10 +62,11 @@
         </div>
     </div>
 
+    {{-- Угоди боје ових медаља, или стави слику медаље --}}
     <div class="col-sm-4  justify-content-center justify-content-middle ">
-        <div class="bg-warning" style="height:30px; width:300px" > </div><br>
-        <div class="bg-warning" style="height:30px; width:300px"></div><br>
-        <div class="bg-warning" style="height:30px; width:300px"></div>
+        <div class="bg-warning text-center" style="height:30px; width:300px" > 13 </div><br>
+        <div class="bg-warning text-center" style="height:30px; width:300px"> 2</div><br>
+        <div class="bg-warning text-center" style="height:30px; width:300px"> 123</div>
     </div>
 </div>
 
