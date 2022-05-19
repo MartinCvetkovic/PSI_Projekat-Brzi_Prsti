ca2c97cf5dd92b0dea0de0d2e38ecc56c60748fb
tp39-19Student <tp190039d@student.etf.bg.ac.rs>
2022-05-20 00:46:42 +0200

    Uvezen jQuery, Bootstrap dependency uvezen lokalno

--- Implementacija/resources/views/template.blade.php
+++ Implementacija/resources/views/template.blade.php
@@ -10,8 +10,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--    Ako local verzija nece, ovo su CDN-ovi od ranije
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    -->
+
+    <!-- Localne verzije Bootstrap i jQuery (public/css/... i public/js/...) -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap/bootstrap.min.css')}}">
+    <script src="{{asset('js/bootstrap/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('js/jQuery/jquery-3.6.0.min.js')}}"></script>
     <link rel="stylesheet" href={{asset("css/templateStyle.css")}}>
     <title>Brzi prsti</title>
 </head>
