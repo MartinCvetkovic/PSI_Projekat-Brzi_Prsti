5fd0cbe5051055119806523e52fa183d28d88c82
ikilis <ivanthesavic@gmail.com>
2022-06-08 23:11:54 +0200

    testovi kontrolera za dodavanje/izmene tekstova

--- Implementacija/tests/Unit/TextControllerTest.php
+++ Implementacija/tests/Unit/TextControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 
 use App\Models\User;
 use App\Models\LeaderboardModel;
+use App\Models\TextModel;
 use App\Models\UserModel;
 use Tests\TestCase;
 
@@ -20,7 +21,6 @@ class TextControllerTest extends TestCase
 
         $response->assertViewIs("rank_list");
     }
-
     /*
         Provera ubacivanja u bazu podataka
     */
@@ -76,12 +76,195 @@ class TextControllerTest extends TestCase
         $response->assertRedirect("/");
 
     }
-    public function test_prisup_stvaranju()
+     /*
+        Provera pristupa admina stranici za izmenu  tekstova
+    */
+    public function test_prisup_stvaranju_admin()
     {
         $user = UserModel::where("tip",2)->first();
  
         $response = $this->actingAs($user)->get("/texts/create");
-        $response->assertStatus(200);
         $response->assertViewIs("texts\create");
     }
+     /*
+        Provera pristupa, moderatora stranici za izmenu  tekstova
+    */
+    public function test_prisup_stvaranju_mod()
+    {
+        $user = UserModel::where("tip",1)->first();
+ 
+        $response = $this->actingAs($user)->get("/texts/create");
+
+        $response->assertViewIs("texts\create");
+    }
+    // Ne redirektuje na pocetnu stranicu
+     /*
+        Provera pristupa registrovanog (tip==0) stranici za izmenu  tekstova
+    */
+    // public function test_prisup_stvaranju_registrovan()
+    // {
+    //     $user = UserModel::where("tip",0)->first();
+ 
+    //     $response = $this->actingAs($user)->get("/texts/create");
+    //     $response->assertRedirect("/");
+    // }
+    /*
+        Kreiranje sa tezinom < 10
+    */
+    public function test_kreiranje_tezina_levoOd_intervala()
+    {
+        $user = UserModel::where("tip",2)->first();
+ 
+        $response = $this->actingAs($user)
+                    ->post("/texts/store",["sadrzaj"=>"value", "tezina"=>"-121","idKat"=>"2"])
+                    ->assertSessionHasErrors("tezina")
+                    ->assertStatus(302);
+
+        $this->assertDatabaseMissing('tekst', [ 'tezina' => "-121"]);
+    }
+    /*
+        Kreiranje sa tezinom > 10
+    */
+    public function test_kreiranje_tezina_desnoOd_intervala()
+    {
+        $user = UserModel::where("tip",2)->first();
+ 
+        $response = $this->actingAs($user)
+                    ->post("/texts/store",["sadrzaj"=>"value", "tezina"=>"121","idKat"=>"2"])
+                    ->assertSessionHasErrors("tezina")
+                    ->assertStatus(302);
+
+        $this->assertDatabaseMissing('tekst', [ 'tezina' => "121"]);
+    }
+    /*
+        Kreiranje sa tezinom koja nije broj
+    */
+    public function test_kreiranje_tezina_neBroj()
+    {
+        $user = UserModel::where("tip",2)->first();
+ 
+        $response = $this->actingAs($user)
+                    ->post("/texts/store",["sadrzaj"=>"value", "tezina"=>"tesko","idKat"=>"2"])
+                    ->assertSessionHasErrors("tezina")
+                    ->assertStatus(302);
+
+        $this->assertDatabaseMissing('tekst', [ 'tezina' => "tesko"]);
+    }
+    /*
+        Testiranje prazan sadrzaj
+    */
+    public function test_kreiranje_prazan_sadrzaj()
+    {
+        $user = UserModel::where("tip",1)->first();
+ 
+        $response = $this->actingAs($user)
+                    ->post("/texts/store",["sadrzaj"=>"", "tezina"=>"10","idKat"=>"2"])
+                    ->assertSessionHasErrors("sadrzaj")
+                    ->assertStatus(302);
+
+        $this->assertDatabaseMissing('tekst', [ 'sadrzaj' => ""]);
+    }
+    /*
+        Uspesno dodavanje teksta
+    */
+    public function test_kreiranje_uspeh()
+    {
+        $user = UserModel::where("tip",1)->first();
+ 
+        $response = $this->actingAs($user)
+                        ->post("/texts/store",["sadrzaj"=>"value", "tezina"=>"1","idKat"=>"2"])
+                        ->assertStatus(302);
+        $response->assertRedirect("/texts")->with("success");
+    }
+    /*
+        Kreiranje sa tezinom < 10
+    */
+    public function test_azuriranje_tezina_levoOd_intervala()
+    {
+        $user = UserModel::where("tip",1)->first();
+        $tekst = TextModel::first();
+ 
+        $response = $this->actingAs($user)
+        ->post("/texts/update",["id"=>$tekst->id,"sadrzaj"=>"value", "tezina"=>"-121","idKat"=>"2"])
+                    ->assertSessionHasErrors("tezina")
+                    ->assertStatus(302);
+
+        $this->assertDatabaseMissing('tekst', [ 'tezina' => "-121"]);
+    }
+    /*
+        Kreiranje sa tezinom > 10
+    */
+    public function test_azuriranje_tezina_desnoOd_intervala()
+    {
+        $user = UserModel::where("tip",1)->first();
+        $tekst = TextModel::first();
+ 
+        $response = $this->actingAs($user)
+        ->post("/texts/update",["id"=>$tekst->id,"sadrzaj"=>"value", "tezina"=>"121","idKat"=>"2"])
+                    ->assertSessionHasErrors("tezina")
+                    ->assertStatus(302);
+
+        $this->assertDatabaseMissing('tekst', [ 'tezina' => "121"]);
+    }
+    /*
+        Azuriranje sa tezinom koja nije broj
+    */
+    public function test_azuriranje_tezina_nijeBroj()
+    {
+        $user = UserModel::where("tip",1)->first();
+        $tekst = TextModel::first();
+ 
+ 
+        $response = $this->actingAs($user)
+        ->post("/texts/update",["id"=>$tekst->id,"sadrzaj"=>"value", "tezina"=>"tesko","idKat"=>"2"])
+                    ->assertSessionHasErrors("tezina")
+                    ->assertStatus(302);
+
+        $this->assertDatabaseMissing('tekst', [ 'tezina' => "tesko"]);
+    }
+    /*
+        Azuriranje  prazan sadrzaj
+    */
+    public function test_auziranje_prazan_sadrzaj()
+    {
+        $user = UserModel::where("tip",1)->first();
+        $tekst = TextModel::first();
+        
+        $response = $this->actingAs($user)
+        ->post("/texts/update",["id"=>$tekst->id,"sadrzaj"=>"", "tezina"=>"1","idKat"=>"2"])
+                    ->assertSessionHasErrors("sadrzaj")
+                    ->assertStatus(302);
+
+        $this->assertDatabaseMissing('tekst', [ 'sadrzaj' => ""]);
+    }
+    /*
+        Azuriranje  uspesno
+    */
+    public function test_auziranje_uspeh()
+    {
+        $user = UserModel::where("tip",1)->first();
+        $tekst = TextModel::first();
+ 
+  
+        $response = $this->actingAs($user)
+                    ->post("/texts/update",["id"=>$tekst->id,"sadrzaj"=>"novi sadrzaj", "tezina"=>"1","idKat"=>"2"])
+                    ->assertStatus(302);
+
+        $this->assertDatabaseHas('tekst', [ 'sadrzaj' => "novi sadrzaj", "tezina"=>"1"]);
+        $response->assertRedirect("/texts")->with("success");
+    }
+
+    /*
+        Pristup editu nepostojeceg teksta, ne prolazi
+    */
+    // public function test_global_edit_nepostojeceg()
+    // {
+    //     $user = UserModel::where("tip",2)->first();
+ 
+ 
+    //     $response = $this->actingAs($user)->get('/tekst/edit/-1');
+
+    //     $response->assertRedirect("/");
+    // }
+
 }
