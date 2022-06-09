65013aa05d336dc1f206d7b0d717985ab2637517
MartinCvetkovic <martincvetkovic2000@yahoo.com>
2022-06-09 11:52:22 +0200

    Zavrseni moji testovi, ima 3 risked, sredjujem kad dobijem vise info

--- Implementacija/tests/Unit/BaseControllerTest.php
+++ Implementacija/tests/Unit/BaseControllerTest.php
@@ -10,4 +10,37 @@ class BaseControllerTest extends TestCase
     public function test_home_page() {
         $this->get('/')->assertViewIs('home');
     }
+
+    public function test_soloKucanjeRandom()
+    {
+        $this->get('/solo')->assertViewIs('solo');
+    }
+
+    public function test_soloKucanjeId()
+    {
+        $this->get('/solo/13')->assertViewIs('solo');
+    }
+
+    public function test_soloEndPost()
+    {
+        /*$response = $this->post('/soloEnd', [
+            'id' => 13,
+            'speed' => 15,
+            'best_speed' => 13,
+            'best_position' => 2
+        ]);
+
+        $response->assertRedirect(route('solo_kucanje_rezultati'));*/
+        //$this->assertDatabaseHas('korisnik', ['username' => 'testuserreg1']);
+    }
+
+    public function test_soloEndGet()
+    {
+        $this->get('/soloEnd')->assertRedirect('/');
+    }
+
+    public function test_soloKucanjePrikazRezultata()
+    {
+        //$this->get('/soloResults')->assertViewIs('solo_kucanje_rezultati');
+    }
 }
